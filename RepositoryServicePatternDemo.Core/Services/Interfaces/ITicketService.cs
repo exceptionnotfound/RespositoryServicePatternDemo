054d3b3b06a173d017893d8ef0021c1b5a3bb998
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-02-12 09:45:30 -0700

    Refactored the service interfaces so they don't inherit from the Repository interfaces any more.

--- RepositoryServicePatternDemo.Core/Services/Interfaces/ITicketService.cs
+++ RepositoryServicePatternDemo.Core/Services/Interfaces/ITicketService.cs
@@ -1,9 +1,13 @@
-﻿using RepositoryServicePatternDemo.Core.Repositories.Interfaces;
+﻿using RepositoryServicePatternDemo.Core.Models;
+using RepositoryServicePatternDemo.Core.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RepositoryServicePatternDemo.Core.Services.Interfaces
 {
-    public interface ITicketService : ITicketRepository { }
+    public interface ITicketService
+    {
+        List<Ticket> GetAllSold();
+    }
 }
